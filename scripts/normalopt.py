a179dbc26a53c572c086fefe2aa522ec52fb3dcc
Dan Yamins <dyamins@gmail.com>
2016-06-02 10:19:15 -0400

    small chances

--- scripts/normalopt.py
+++ scripts/normalopt.py
@@ -1,11 +1,11 @@
 import os
 
-def main(ind, gpu=0):
+def main(ind, srcdir, savedir, gpu=0):
     learningrate = 1.
     for i in range(10):
         seed = ind
         experiment_id = "seed:%d_learningrate:%f" % (seed, learningrate)
-        cmd_tmp = """CUDA_VISIBLE_DEVICES=%d python /om/user/yamins/src/curiosity/curiosity/sandbox/normal_encoder_opt_source.py %s --seed %d --learningrate=%f --savedir=/om/user/yamins/tensorflow_checkpoint_cache"""
-        cmd = cmd_tmp % (gpu, experiment_id, seed, learningrate)
+        cmd_tmp = """CUDA_VISIBLE_DEVICES=%d python %s/curiosity/curiosity/sandbox/normal_encoder_opt_source.py %s --seed %d --learningrate=%f --savedir=%s"""
+        cmd = cmd_tmp % (gpu, srcdir, experiment_id, seed, learningrate, savedir)
         os.system(cmd)
         learningrate = learningrate / 2.
--- scripts/normalopt.sh
+++ scripts/normalopt.sh
@@ -4,4 +4,4 @@ export IND=$SLURM_ARRAY_TASK_ID
 cd /home/yamins
 python make_tunnel.py
 cd /om/user/yamins/src/curiosity/scripts
-python -c "import normalopt; normalopt.main($IND)"
+python -c "import normalopt; normalopt.main($IND, '/om/user/yamins/src', '/om/user/yamins/tensorflow_checkpoint_cache')"
