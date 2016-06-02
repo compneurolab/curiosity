495cf21e91c2782940fdbb8912c7248f4b860823
Daniel Yamins <dyamins@gmail.com>
2016-06-02 10:01:04 -0400

    minor

--- scripts/run_normalopt.sh
+++ scripts/run_normalopt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH --error=/om/user/yamins/src/curiosity/scripts/normalopt_logs/%a.error
-#SBATCH --output=/om/user/yamins/src/curiousity/scripts/normalopt_logs/%a.out
+#SBATCH --output=/om/user/yamins/src/curiosity/scripts/normalopt_logs/%a.out
 srun -N1 /om/user/yamins/src/curiosity/scripts/normalopt.sh
