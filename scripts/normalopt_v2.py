aa1d33e669fa8a845ac7ba2583d5bcffd78d6f42
Dan Yamins <dyamins@gmail.com>
2016-06-08 11:54:46 -0400

    minor

--- scripts/normalopt_v2.py
+++ scripts/normalopt_v2.py
@@ -9,14 +9,14 @@ def main(ind, dbname, colname, srcdir, savedir, gpu=0, script='normal_encoder_op
             seed = ind * num_tries + n
             print('Seed: %d' % seed)
             experiment_id = "seed:%d_learningrate:%f_decaystep:%d_decayrate:%f" % (seed, learningrate, decaystep, decayrate)
-            if conn[dbname][colname].find({'experiment_id': experiment_id}).count() > 50:
+            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps:
                 print('Breaking out at %s' % experiment_id)
                 break
             cmd_tmp = """CUDA_VISIBLE_DEVICES=%d python %s/curiosity/curiosity/sandbox/%s %s %s %s --seed %d --learningrate=%f --savedir=%s --decaystep=%d --decayrate=%f --num_train_steps=%d --erase_earlier=%d"""
             cmd = cmd_tmp % (gpu, srcdir, script, dbname, colname, experiment_id, seed, learningrate, savedir, decaystep, decayrate, num_train_steps, erase_earlier)
             print('CMD: %s' % cmd)
             os.system(cmd)
-            if conn[dbname][colname].find({'experiment_id': experiment_id}).count() > 25:
+            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps:
                 print('Breaking out at %s due to enough steps' % experiment_id)
                 break
             elif conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step') == [-1]:
