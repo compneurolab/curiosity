cf30e4597f1935f55e92c03ccfac57457e6a6a93
Dan Yamins <dyamins@gmail.com>
2016-06-08 12:08:31 -0400

    minor

--- scripts/normalopt_v2.py
+++ scripts/normalopt_v2.py
@@ -3,20 +3,22 @@ import pymongo as pm
 conn = pm.MongoClient(port=29101)
 
 num_tries = 10
+bsize = 64
+test_freq = 20
 def main(ind, dbname, colname, srcdir, savedir, gpu=0, script='normal_encoder_opt_source.py', decayrate=0.95, decaystep=100000, num_train_steps=2048000, erase_earlier=0):
     for n in range(num_tries):
         for learningrate in [5., 1., .75, .5]:
             seed = ind * num_tries + n
             print('Seed: %d' % seed)
             experiment_id = "seed:%d_learningrate:%f_decaystep:%d_decayrate:%f" % (seed, learningrate, decaystep, decayrate)
-            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps:
+            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps/bsize - 5 * test_freq:
                 print('Breaking out at %s' % experiment_id)
                 break
             cmd_tmp = """CUDA_VISIBLE_DEVICES=%d python %s/curiosity/curiosity/sandbox/%s %s %s %s --seed %d --learningrate=%f --savedir=%s --decaystep=%d --decayrate=%f --num_train_steps=%d --erase_earlier=%d"""
             cmd = cmd_tmp % (gpu, srcdir, script, dbname, colname, experiment_id, seed, learningrate, savedir, decaystep, decayrate, num_train_steps, erase_earlier)
             print('CMD: %s' % cmd)
             os.system(cmd)
-            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps:
+            if max(conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step')) >= num_train_steps/bsize - 5 * test_freq:
                 print('Breaking out at %s due to enough steps' % experiment_id)
                 break
             elif conn[dbname][colname].find({'experiment_id': experiment_id}).distinct('step') == [-1]:
