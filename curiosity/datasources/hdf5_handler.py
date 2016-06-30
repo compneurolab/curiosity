858e4d5b0f2874d654076f082964b7e3513c893e
Dan Yamins <dyamins@gmail.com>
2016-06-30 12:26:58 -0400

    recursive gettingin hdf5 handler

--- curiosity/datasources/hdf5_handler.py
+++ curiosity/datasources/hdf5_handler.py
@@ -8,44 +8,68 @@ import numpy as np
 import h5py
 import json
 import zmq
+import argparse
 
-from curiosity.utils.io import (handle_sock,
-                                send_array, 
-                                recv_array)
+from curiosity.utils.io import send_array
 
-
-ctx = zmq.Context()
-sock2 = ctx.socket(zmq.REP)
-sock2.bind('tcp://18.93.3.135:23044')
 
 file = None
 
-def initialize(path)
+def initialize(path):
     global file
     if file is None:
+        print('initializing...')
         file = h5py.File(path, mode='r')
+        print('... on %s' % file.filename)
     else:
         assert path == file.filename, (path, file.filename)
+        print('Already initialized to %s' % file.filename)
 
 
-while True:
-    msg = sock2.recv_json()
-    print(msg)
-    initialize(msg['path'])
-    keys = msg['keys']
-    if 'batch_size' in msg:
-        N = file[keys[0]].shape[0]
-        bn = msg['batch_num']
-        batch_size = msg['batch_size']
-        start = (bn * bsize) % N
-        end = ((bn + 1) * bsize - 1) % N + 1
-        sl = slice(start, end)
+def rget(f, k):
+    if isinstance(k, str):
+        return f[k]
     else:
-        sl = slice(None)
-    print("Sending batch %d" % bn)
-    for ind, k in enumerate(keys):
-        data = file[k][sl]
-        if ind < len(keys) - 1:
-            send_array(sock2, data, flags=zmq.SNDMORE)
+        assert isinstance(k, list), k
+        r = f
+        for _k in k:
+            r = r[_k]
+        return r
+
+
+def main(host, port):
+    ctx = zmq.Context()
+    sock2 = ctx.socket(zmq.REP)
+    sockstr = 'tcp://%s:%d' % (host, port)
+    sock2.bind(sockstr)
+    print('Bound to %s' % sockstr)
+
+    while True:
+        msg = sock2.recv_json()
+        print(msg)
+        initialize(msg['path'])
+        keys = msg['keys']
+        if 'batch_size' in msg:
+            N = rget(file, keys[0]).shape[0]
+            bn = msg['batch_num']
+            batch_size = msg['batch_size']
+            start = (bn * batch_size) % N
+            end = ((bn + 1) * batch_size - 1) % N + 1
+            sl = slice(start, end)
         else:
-            send_array(sock2, data)
+            sl = slice(None)
+        print("Sending batch %d" % bn)
+        for ind, k in enumerate(keys):
+            data = rget(file, k)[sl]
+            if ind < len(keys) - 1:
+                send_array(sock2, data, flags=zmq.SNDMORE)
+            else:
+                send_array(sock2, data)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', type=str, help="host")
+    parser.add_argument('port', type=int, help="port")
+    args = vars(parser.parse_args())
+    main(**args)
