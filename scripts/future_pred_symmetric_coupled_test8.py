4a9d62a024ae6378e1785e6e888012eb5d73b838
Dan Yamins <dyamins@gmail.com>
2016-07-17 22:02:00 -0400

    minor

--- scripts/future_pred_symmetric_coupled_test8.py
+++ scripts/future_pred_symmetric_coupled_test8.py
@@ -9,7 +9,7 @@ import curiosity.datasources.images_futures_and_actions as datasource
 
 dbname = 'threeworld_future_pred'
 colname = 'test_symmetric_coupled_withbelow'
-experiment_id = 'test0_dataset2_lr01'
+experiment_id = 'test2_dataset2_lr1'
 model_func = modelsource.get_model
 model_func_kwargs = {"host": "18.93.3.135",
                      "port": 23045,
@@ -24,7 +24,7 @@ slippage = 0
 SKDATA_ROOT = os.environ['SKDATA_ROOT']
 CODE_ROOT = os.environ['CODE_ROOT']
 cfgfile = os.path.join(CODE_ROOT, 
-                       'curiosity/curiosity/configs/future_pred_test0.cfg')
+                       'curiosity/curiosity/configs/future_pred_test2.cfg')
 savedir = os.path.join(SKDATA_ROOT, 'futurepredopt')
 erase_earlier = 3
 decaystep=1024000
@@ -42,6 +42,6 @@ base.run(dbname,
          cfgfile=cfgfile,
          savedir=savedir,
          erase_earlier=erase_earlier,
-         base_learningrate=0.1,
+         base_learningrate=1.,
          loss_threshold=10000,
          decaystep=decaystep)
