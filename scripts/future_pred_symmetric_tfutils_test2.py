8458647551118b31152b8c5e52226b2c17ba446c
Nick Haber <nhaber@stanford.edu>
2017-01-27 20:29:23 -0800

    Add new validation pipeline w saved images

--- scripts/future_pred_symmetric_tfutils_test2.py
+++ scripts/future_pred_symmetric_tfutils_test2.py
@@ -37,23 +37,69 @@ seed = 0
 rng = np.random.RandomState(seed=seed)
 
 
+
+def get_current_predicted_future_action(inputs, outputs, num_to_save = 1, **loss_params):
+    '''
+    Gives you input tensors and output tensors.
+
+    Assumes to_extract has an inputs field (with list of arguments) and outputs field (with pairs of arguments -- assuming outputs is a dict of dicts)
+    '''
+    futures = inputs['future_images'][:num_to_save]
+    predictions = outputs['pred']['pred0'][:num_to_save]
+    actions = inputs['actions'][:num_to_save]
+    currents = inputs['images'][:num_to_save]
+    futures = tf.cast(futures, tf.uint8)
+    predictions = tf.cast(tf.multiply(predictions, 255), tf.uint8)
+    currents = tf.cast(currents, tf.uint8)
+    retval = {'prediction' : predictions, 'future_images' : futures, 'current_images': currents, 'actions' : actions}
+    retval.update(get_loss_by_layer(inputs, outputs, **loss_params))
+    return retval
+
+def mean_losses_keep_rest(step_results):
+    retval = {}
+    keys = step_results[0].keys()
+    for k in keys:
+        plucked = [d[k] for d in step_results]
+        if 'loss' in k:
+            retval[k] = np.mean(plucked)
+        else:
+            retval[k] = plucked
+    return retval
+
+
+def get_loss_by_layer(inputs, outputs, **loss_params):
+    retval = {}
+    encode_depth = len(outputs['pred']) - 1
+    for i in range(0, encode_depth + 1):
+        tv = outputs['future']['future' + str(i)]
+        pred = outputs['pred']['pred' + str(i)]
+        my_shape = tv.get_shape().as_list()
+        norm = (my_shape[1]**2) * my_shape[0] * my_shape[-1]
+        retval['loss' + str(i)] = tf.nn.l2_loss(pred - tv) / norm
+    return retval
+
+
+
 params = {
 	'save_params' : {
 	    'host': 'localhost',
         'port': 27017,
         'dbname': 'future_pred_test',
         'collname': 'future_pred_symmetric',
-        'exp_id': 'test12_sepval',
-        'save_valid_freq': 3000,
+        'exp_id': 'test18_nominmax',
+        'save_valid_freq': 500,
         'save_filters_freq': 30000,
-        'cache_filters_freq': 3000
+        'cache_filters_freq': 500,
+        'save_initial_filters' : False,
+        'save_to_gfs': ['actions', 'prediction', 'future_images', 'current_images']
 	},
 
 	'model_params' : {
 		'func' : modelsource.model_tfutils_fpd_compatible,
 		'rng' : None,
 		'cfg' : cfg,
-		'slippage' : 0
+		'slippage' : 0,
+        'min_max_end' : False
 	},
 
 	'train_params': {
@@ -98,22 +144,24 @@ params = {
                 'data_path': VALIDATION_DATA_PATH,  # path to image database
                 'random_time': False,
                 'crop_size': [IMAGE_SIZE_CROP, IMAGE_SIZE_CROP],  # size after cropping an image
-            	'min_time_difference': 1,
-            	'batch_size': 128,
+                'min_time_difference': 1,
+                'batch_size': 128,
             },
             'queue_params': {
                 'queue_type': 'random',
                 'batch_size': BATCH_SIZE,
                 'n_threads': 1,
                 'seed': 0,
-            		'capacity': BATCH_SIZE * 100,
+              'capacity': BATCH_SIZE * 100,
             },
-	    'targets': {
-                'func': modelsource.loss_agg_for_validation,
-                'targets': [],
+        'targets': {
+                'func': get_current_predicted_future_action,
+                'targets' : [],
+                'num_to_save' : 10
             },
-	    'agg_func': utils.mean_dict,
-            'num_steps': 1 # N_VAL // BATCH_SIZE + 1,
+        'agg_func' : mean_losses_keep_rest,
+        # 'agg_func': utils.mean_dict,
+        'num_steps': 2 # N_VAL // BATCH_SIZE + 1,
             #'agg_func': lambda x: {k: np.mean(v) for k, v in x.items()},
             #'online_agg_func': online_agg
         }
