7f86c0c49d844855c426f86e82cc18686c8346ac
Damian Mrowca <mrowca@gmx.de>
2017-02-01 19:24:56 -0800

    initial action prediction commit

---
+++ scripts/tfutils_tests/pull_images.py
@@ -0,0 +1,63 @@
+'''
+A script for accessing visualization data (saving images at validation steps during training) and saving them to a local directory.
+'''
+import pymongo as pm
+import pickle
+import os
+import gridfs
+import cPickle
+import numpy as np
+from PIL import Image
+
+dbname = 'acion_pred'
+collname = 'action_pred_symmetric'
+port = 27017
+exp_id = 'test1'
+save_loc = '/home/mrowca/val_data'
+save_fn = os.path.join(save_loc, exp_id + '.p')
+target_name = 'valid0'
+
+conn = pm.MongoClient(port = 27017)
+coll = conn[dbname][collname + '.files']
+print('experiments')
+print(coll.distinct('exp_id'))
+cur = coll.find({'exp_id' : exp_id})
+
+q = {'exp_id' : exp_id, 'validation_results' : {'$exists' : True}}
+val_steps = coll.find(q)
+val_count = val_steps.count()
+print('num val steps so far')
+print(val_count)
+
+saved_data = {}
+
+for val_num in range(val_count):
+	idx = val_steps[val_num]['_id']
+	fn = coll.find({'item_for' : idx})[0]['filename']
+	fs = gridfs.GridFS(coll.database, collname)
+	fh = fs.get_last_version(fn)
+	saved_data[val_num] = cPickle.loads(fh.read())['validation_results']
+	fh.close()
+
+exp_dir = os.path.join(save_loc, exp_id)
+if not os.path.exists(exp_dir):
+	os.mkdir(exp_dir)
+
+for val_num, val_data in saved_data.iteritems():
+	val_dir = os.path.join(exp_dir, 'val_' + str(val_num))
+	if not os.path.exists(val_dir):
+		os.mkdir(val_dir)
+	for tgt_desc, tgt in val_data[target_name].iteritems():
+		tgt_images = [arr for step_results in tgt for arr in step_results]
+		for (instance_num, arr) in enumerate(tgt_images):
+			instance_dir = os.path.join(val_dir, 'instance_' + str(instance_num))
+			if not os.path.exists(instance_dir):
+				os.mkdir(instance_dir)
+			#just save in human-readable form if not a 3-array
+			if len(arr.shape) != 3:
+				fn = os.path.join(instance_dir, tgt_desc + '_' + str(instance_num) + '.txt')
+				np.savetxt(fn, arr)
+			else:
+				fn = os.path.join(instance_dir, tgt_desc + '_' + str(instance_num) + '.jpeg')
+				im = Image.fromarray(arr)
+				im.save(fn)
