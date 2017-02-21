3d9c67f56dfca5b87d9a1460e0835db92c06cd58
Nick Haber <nhaber@stanford.edu>
2017-02-21 13:10:36 -0800

    Add alternate naive architectures

--- curiosity/models/asymmetric_modularized.py
+++ curiosity/models/asymmetric_modularized.py
@@ -5,11 +5,11 @@ Takes a cfg passed in form params.
 
 import numpy as np
 import tensorflow as tf
-import zmq
+
 
 from curiosity.models.model_building_blocks import ConvNetwithBypasses
 
-def feedforward_conv_loop(input_node, m, cfg, reuse_weights = False):
+def feedforward_conv_loop(input_node, m, cfg, bypass_nodes = None, reuse_weights = False, batch_normalize = False, no_nonlinearity_end = False):
 	m.output = input_node
 	encode_nodes = [input_node]
 	#encoding
@@ -17,18 +17,43 @@ def feedforward_conv_loop(input_node, m, cfg, reuse_weights = False):
 	print('Encode depth: %d' % encode_depth)
 	cfs0 = None
 
+	if bypass_nodes is None:
+		bypass_nodes = [m.output]
+
 	for i in range(1, encode_depth + 1):
 	#not sure this usage ConvNet class creates exactly the params that we want to have, specifically in the 'input' field, but should give us an accurate record of this network's configuration
 		with tf.variable_scope('encode' + str(i)) as scope:
 			if reuse_weights:
 				scope.reuse_variables()
 
-			with tf.contrib.framework.arg_scope([m.conv], init='trunc_norm', stddev=.01, bias=0, activation='relu'):
+			bypass = cfg['encode'][i].get('bypass')
+			if bypass:
+				if type(bypass) == list:
+					bypass_node = [bypass_nodes[bp] for bp in bypass]
+				else:
+					bypass_node = bypass_nodes[bypass]
+				m.add_bypass(bypass_node)
+
+			bn = cfg['encode'][i]['conv'].get('batch_normalize')
+			if bn:
+				norm_it = bn
+			else:
+				norm_it = batch_normalize
+
+
+
+			with tf.contrib.framework.arg_scope([m.conv], init='trunc_norm', stddev=.01, bias=0, batch_normalize = norm_it):
 			    cfs = cfg['encode'][i]['conv']['filter_size']
 			    cfs0 = cfs
 			    nf = cfg['encode'][i]['conv']['num_filters']
 			    cs = cfg['encode'][i]['conv']['stride']
-			    m.conv(nf, cfs, cs)
+			    print('conv shape to shape')
+			    print(m.output)
+			    if no_nonlinearity_end and i == encode_depth:
+			    	m.conv(nf, cfs, cs, activation = None)
+			    else:
+			    	m.conv(nf, cfs, cs, activation='relu')
+			    print(m.output)
 	#TODO add print function
 			pool = cfg['encode'][i].get('pool')
 			if pool:
@@ -36,7 +61,7 @@ def feedforward_conv_loop(input_node, m, cfg, reuse_weights = False):
 			    ps = pool['stride']
 			    m.pool(pfs, ps)
 			encode_nodes.append(m.output)
-			print(m.output)
+			bypass_nodes.append(m.output)
 	return encode_nodes
 
 def hidden_loop_with_bypasses(input_node, m, cfg, nodes_for_bypass, reuse_weights = False):
@@ -54,6 +79,7 @@ def hidden_loop_with_bypasses(input_node, m, cfg, nodes_for_bypass, reuse_weight
 			nf = cfg['hidden'][i]['num_features']
 			m.fc(nf, init = 'trunc_norm', activation = 'relu', bias = .01, dropout = None)
 			nodes_for_bypass.append(m.output)
+			print(m.output)
 	return m.output
 
 def hidden_loop(input_node, m, cfg, reuse_weights = False):
@@ -69,7 +95,7 @@ def hidden_loop(input_node, m, cfg, reuse_weights = False):
 			print(m.output)
 	return m.output
 
-def decode_conv_loop(input_node, m, cfg, nodes_for_bypass, reuse_weights = False):
+def decode_conv_loop(input_node, m, cfg, nodes_for_bypass, reuse_weights = False, batch_normalize = False):
 	assert len(input_node.get_shape().as_list()) == 4, len(input_node.get_shape().as_list())
 	m.output = input_node
 	decode_depth = cfg['decode_depth']
@@ -81,16 +107,32 @@ def decode_conv_loop(input_node, m, cfg, nodes_for_bypass, reuse_weights = False
 			m.resize_images(ds)
 			bypass = cfg['decode'][i].get('bypass')
 			if bypass:
-				bypass_layer = nodes_for_bypass[bypass]
-				m.add_bypass(bypass_layer)
+				if type(bypass) == list:
+					bypass_node = [nodes_for_bypass[bp] for bp in bypass]
+				else:
+					bypass_node = nodes_for_bypass[bypass]
+				m.add_bypass(bypass_node)
 			nf1 = cfg['decode'][i]['num_filters']
 			cfs = cfg['decode'][i]['filter_size']
-			if i < decode_depth:
-				m.conv(nf1, cfs, 1, init='trunc_norm', stddev=.1, bias=0, activation='relu')
+			print('decode dims')
+			print(m.output.get_shape().as_list())
+
+			bn = cfg['decode'][i].get('batch_normalize')
+			if bn:
+				norm_it = bn
 			else:
-				m.conv(nf1, cfs, 1, init='trunc_norm', stddev=.1, bias=0, activation=None)
-			print(m.output)
+				norm_it = batch_normalize
+
+
+			if i < decode_depth:
+				m.conv(nf1, cfs, 1, init='trunc_norm', stddev=.1, bias=0, activation='relu', batch_normalize = norm_it)
+			else:
+				m.conv(nf1, cfs, 1, init='trunc_norm', stddev=.1, bias=0, activation=None, batch_normalize = False) #assuming we don't want to batch normalize at the end
+			print(m.output.get_shape().as_list())
+			nodes_for_bypass.append(m.output)
 	return m.output
+
+
 
 
 def std_asymmetric_model(inputs, cfg = None, num_channels = 3, T_in = 1, T_out = 1, rng = None, **kwargs):
@@ -125,7 +167,58 @@ def std_asymmetric_model(inputs, cfg = None, num_channels = 3, T_in = 1, T_out =
 
 	return {'pred' : m.output, 'tv' : future_node}, m.params
 
-def asymmetric_with_bottleneck(inputs, cfg = None, num_channels = 3, T_in = 3, T_out = 3, **kwargs):
+def somewhat_less_simple_net(inputs, skip = 1, cfg = None, num_channels = 3, T_in = 3, T_out = 3, batch_normalize = False, **kwargs):
+	actions_sequence = tf.cast(inputs['parsed_actions'], tf.float32)
+	current_node = inputs['images'][:, :, :, :num_channels * T_in]
+	current_node = tf.divide(tf.cast(current_node, tf.float32), 255.)
+	future_node1 = inputs['images'][:, :, :, num_channels * T_in : ]
+	future_node2 = inputs['future_images']
+	future_node = tf.concat(3, [future_node1, future_node2])
+	if skip > 1:
+		print('lengths')
+		c_list = [current_node[:, :, :, num_channels * i : num_channels * (i + 1)] for i in range(0, T_in, skip)]
+		print(len(c_list))
+		current_node = tf.concat(3, c_list)
+		print(current_node.get_shape().as_list())
+		f_list = [future_node[:, :, :, num_channels * i : num_channels * (i + 1)] for i in range(skip - 1, T_out, skip)]
+		print(len(f_list))
+		future_node = tf.concat(3, f_list)
+		print(future_node.get_shape().as_list())
+	assert num_channels * (T_in + T_out) == inputs['images'].get_shape().as_list()[3] + num_channels
+
+	m = ConvNetwithBypasses(**kwargs)
+
+	encode_nodes = feedforward_conv_loop(current_node, m, cfg, reuse_weights = False, batch_normalize = batch_normalize)
+
+	decode_conv_loop(m.output, m, cfg, encode_nodes, reuse_weights = False)
+
+	return {'pred' : m.output, 'tv' : future_node}, m.params
+
+
+def simple_net(inputs, cfg = None, num_channels = 3, T_in = 3, T_out = 3, batch_normalize = False, **kwargs):
+	actions_sequence = tf.cast(inputs['parsed_actions'], tf.float32)
+	current_node = inputs['images'][:, :, :, :num_channels * T_in]
+	current_node = tf.divide(tf.cast(current_node, tf.float32), 255.)
+	future_node = inputs['images'][:, :, :, num_channels * T_in : ]
+	assert num_channels * (T_in + T_out) == inputs['images'].get_shape().as_list()[3]
+
+	m = ConvNetwithBypasses(**kwargs)
+
+	feedforward_conv_loop(current_node, m, cfg, reuse_weights = False, batch_normalize = batch_normalize, no_nonlinearity_end = True)
+
+	return {'pred' : m.output, 'tv' : future_node}, m.params
+
+def nothing_net(inputs, **kwargs):
+	future_node = inputs['images'][:, :, :, num_channels * T_in : ]
+	future_shape = future_node.get_shape().as_list()
+	trivial_ans = tf.zeros(future_shape)
+
+	m = ConvNetwithBypasses(**kwargs)
+
+	return {'pred' : trivial_ans, 'tv' : future_node}, m.params
+
+
+def asymmetric_with_bottleneck(inputs, cfg = None, num_channels = 3, T_in = 3, T_out = 3, batch_normalize = False, **kwargs):
 	actions_sequence = tf.cast(inputs['parsed_actions'], tf.float32)
 	current_node = inputs['images'][:, :, :, :num_channels * T_in]
 	current_node = tf.divide(tf.cast(current_node, tf.float32), 255.)
@@ -135,7 +228,7 @@ def asymmetric_with_bottleneck(inputs, cfg = None, num_channels = 3, T_in = 3, T
 	m = ConvNetwithBypasses(**kwargs)
 
 	#encode
-	encode_nodes = feedforward_conv_loop(current_node, m, cfg, reuse_weights = False)
+	encode_nodes = feedforward_conv_loop(current_node, m, cfg, reuse_weights = False, batch_normalize = batch_normalize)
 	
 	#flatten
 	enc_shape = m.output.get_shape().as_list()
@@ -144,8 +237,9 @@ def asymmetric_with_bottleneck(inputs, cfg = None, num_channels = 3, T_in = 3, T
 	nf = cfg['hidden']['to_repn']['num_features']
 	m.fc(nf, init = 'trunc_norm', activation = 'relu', bias = .01, dropout = None)
 	hidden_depth = cfg['hidden_depth']
-	n_dynamic = cfg['hidden'][hidden_depth - 1]
-	static_node = m.output[:, :, :, n_dynamic : ]
+	n_dynamic = cfg['hidden'][hidden_depth - 1]['num_features']
+	print('num dynamic ' + str(n_dynamic))
+	static_node = m.output[:, n_dynamic : ]
 	nodes_for_bypass = [static_node]
 
 	#hidden
@@ -233,11 +327,13 @@ def compute_diffs_timestep_1(original_image, subsequent_images, num_channels = 3
 
 
 def something_or_nothing_loss_fn(outputs, image, threshold = None, num_channels = 3, **kwargs):
+	print('in loss')
+	print(outputs)
+	print(image)
 	pred = outputs['pred']
 	future_images = tf.cast(outputs['tv'], 'float32')
 	assert threshold is not None
-	T_in = int((image.get_shape().as_list()[-1] -  pred.get_shape().as_list()[-1]) / num_channels)
-	original_image = image[:, :, :, (T_in - 1) * num_channels: T_in * num_channels]
+	original_image = image[:, :, :, T_in * num_channels: T_in * num_channels]
 	original_image = tf.cast(original_image, 'float32')
 	diffs = compute_diffs_timestep_1(original_image, future_images, num_channels = num_channels)
 	#just measure some absolute change relative to a threshold
