9a0d5f669a04138709e2305c60864811013e72c1
Damian Mrowca <mrowca@gmx.de>
2017-02-28 22:37:45 -0800

    latest models

--- scripts/tfutils_tests/action_pred_test5.py
+++ scripts/tfutils_tests/action_pred_test5.py
@@ -30,7 +30,7 @@ NUM_BATCHES_PER_EPOCH = N // OUTPUT_BATCH_SIZE
 IMAGE_SIZE_CROP = 256
 TIME_DIFFERENCE = 5
 seed = 0
-exp_id = 'test81'
+exp_id = 'test83'
 
 rng = np.random.RandomState(seed=seed)
 
