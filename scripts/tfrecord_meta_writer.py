3c7cb3d408f2bdc5743c6cea553a98254ca5658e
Nick Haber <nhaber@stanford.edu>
2017-03-08 17:20:49 -0800

    Modify data provider meta writer

--- scripts/tfrecord_meta_writer.py
+++ scripts/tfrecord_meta_writer.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 import cPickle
 
 data2_dir = '/media/data2/one_world_dataset/tfvaldata'
-meta_loc = os.path.join(data2_dir, 'act_mask_1', 'meta.pkl')
+attr_name = 'pos_squeezed'
+meta_loc = os.path.join(data2_dir, attr_name, 'meta.pkl')
 
-mdat = {'act_mask_1': {'dtype': tf.string, 'shape': []}}
+mdat = {attr_name: {'dtype': tf.string, 'shape': []}}
 
 with open(meta_loc, 'w') as stream:
 	cPickle.dump(mdat, stream)
