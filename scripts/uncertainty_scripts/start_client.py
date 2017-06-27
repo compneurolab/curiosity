2f51fee9f4c2fd4f16049fec52972d9865723649
Nick Haber <haber.nick@gmail.com>
2017-06-27 12:13:51 -0700

    Modify zmq test

--- scripts/uncertainty_scripts/start_client.py
+++ scripts/uncertainty_scripts/start_client.py
@@ -16,5 +16,17 @@ sock = ctx.socket(zmq.REQ)
 sock.connect('tcp://' + addy + ':' + str(23402))
 
 
+
 msg0 = json.dumps({'test' : 0})
 sock.send_json(msg0)
+msg0_back = sock.recv_json()
+
+
+
+msg1 = '0'
+sock.send(msg1)
+
+
+msg0_back = json.loads(msg0_back)
+print(msg0_back)
+
