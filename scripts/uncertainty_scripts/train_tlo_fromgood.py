0e3b696f7126c4a65215887c62e2a7df099d5b88
Nick Haber <haber.nick@gmail.com>
2017-07-14 23:01:38 -0700

    Modify learn-from-interesting-place script

--- scripts/uncertainty_scripts/train_tlo_fromgood.py
+++ scripts/uncertainty_scripts/train_tlo_fromgood.py
@@ -15,7 +15,7 @@ import numpy as np
 import os
 
 NUM_BATCHES_PER_EPOCH = 1e8
-RENDER2_HOST_ADDRESS = '10.102.2.162'
+RENDER1_HOST_ADDRESS = '10.102.2.161'
 
 EXP_ID_OLD = 'tlo'
 EXP_ID_NEW = 'tlo_fromgood1'
@@ -48,7 +48,7 @@ env_cfg = [
 
 
 params = {
-	'allow_growth' = True,
+	'allow_growth' : True,
 	'load_params' : {
 		'host' : 'localhost',
 		'port' : 27017,
@@ -56,7 +56,7 @@ params = {
 		'collname' : 'uniform_action',
 		'exp_id' : EXP_ID_OLD,
 		'do_restore' : True,
-		'query' : {'saved_filters' : True, 'step' : 3300000},
+		'query' : {'saved_filters' : True, 'step' : 3340000},
 		'load_param_dict' : None
 	},
 
@@ -104,7 +104,7 @@ params = {
 					STATE_DESC : (64, 64)
 				},
 			'USE_TDW' : True,
-			'host_address' : RENDER2_HOST_ADDRESS,
+			'host_address' : RENDER1_HOST_ADDRESS,
 			'message_memory_len' : 2,
 			'action_memory_len' : 2
 		},
