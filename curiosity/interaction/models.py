Fix broadcasting l2_loss bug

curiosity/interaction/models.py
@@ -244,7 +244,7 @@ def action_softmax_loss(prediction, tv, num_classes = 21, min_value = -1., max_v
 	tv = float(num_classes) * (tv - min_value) / (max_value - min_value)
 	tv = tf.cast(tv, tf.int32)
 	loss_per_example = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-				labels = tv, logits = pred), axis = 1)
+				labels = tv, logits = pred), axis = 1, keep_dims = True)
 	loss = tf.reduce_mean(loss_per_example)
 	return loss_per_example, loss
 
@@ -917,12 +917,12 @@ class LatentSpaceWorldModel(object):
             if loss_type == 'both_l2':
                 act_post_flat = flatten(self.action_post)
 		diff = self.act_pred - act_post_flat
-		self.act_loss_per_example = tf.reduce_sum(diff * diff, axis = 1) / 2. * act_loss_factor
+		self.act_loss_per_example = tf.reduce_sum(diff * diff, axis = 1, keep_dims = True) / 2. * act_loss_factor
                 self.act_loss = tf.reduce_mean(self.act_loss_per_example)
             elif loss_type == 'one_l2':
                 act_post_flat = self.action_post[:, -1]
 		diff = self.act_pred - act_post_flat
-		self.act_loss_per_example = tf.reduce_sum(diff * diff, axis = 1) / 2. * act_loss_factor
+		self.act_loss_per_example = tf.reduce_sum(diff * diff, axis = 1, keep_dims = True) / 2. * act_loss_factor
                 self.act_loss = tf.reduce_mean(self.act_loss_per_example)
             elif loss_type == 'one_cat':
 		print('cat')
@@ -1121,7 +1121,32 @@ class ForceMagSquareWorldModel:
                 self.action = tf.placeholder(tf.float32, [None] + cfg['action_shape'])
 		self.action_post = tf.placeholder(tf.float32, [None] + cfg['action_shape'])
 		force = self.action_post[:, -1, 2:]
-		self.square_force_magnitude = tf.reduce_sum(force * force, axis = 1) / 2.
+		self.square_force_magnitude = tf.reduce_sum(force * force, axis = 1, keep_dims = True) / 2.
+
+class SimpleForceUncertaintyModel:
+	def __init__(self, cfg, world_model):
+		with tf.variable_scope('uncertainty_model'):
+			m = ConvNetwithBypasses()
+			self.action_sample = ac = world_model.action[:, -1]
+			self.true_loss = world_model.square_force_magnitude
+			self.obj_there = x = tf.placeholder(tf.int32, [None])
+			x = tf.cast(x, tf.float32)
+			x = tf.expand_dims(x, 1)
+			self.oh_my_god = x = x * ac * ac
+			self.ans = tf.reduce_sum(x[:, 2:], axis = 1)
+			if cfg.get('use_ans', False):
+				x = self.ans
+				x = tf.expand_dims(x, 1)
+			self.estimated_world_loss = tf.squeeze(hidden_loop_with_bypasses(x, m, cfg, reuse_weights = False, train = True))
+			self.uncertainty_loss = l2_loss(self.true_loss, self.estimated_world_loss, {'loss_factor' : 1 / 32.})
+			self.rng = np.random.RandomState(0)
+			self.var_list = [var for var in tf.global_variables() if 'uncertainty_model' in var.name]
+
+	def act(self, sess, action_sample, state):
+		chosen_idx = self.rng.randint(len(action_sample))
+		print('random act!')
+		return action_sample[chosen_idx], -1., None
+
 
 
 class UncertaintyModel:
@@ -1134,6 +1159,7 @@ class UncertaintyModel:
             if cfg.get('only_model_ego', False):
                 ac = ac[:, :2]
             self.true_loss = tr_loss = cfg['wm_loss']['func'](world_model, **cfg['wm_loss']['kwargs'])
+            assert len(self.true_loss.get_shape().as_list()) == 2
             if cfg.get('use_world_encoding', False):
                 self.encoded = x = world_model.encoding_i
             else: