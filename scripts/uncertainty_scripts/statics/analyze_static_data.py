92bc6063d4f4d14f27a66457a99aee37bef9ea58
Nick Haber <nhaber@stanford.edu>
2017-09-12 14:51:34 -0700

    Add more_info and big force stuffs

--- scripts/uncertainty_scripts/statics/analyze_static_data.py
+++ scripts/uncertainty_scripts/statics/analyze_static_data.py
@@ -21,24 +21,31 @@ import numpy as np
 
 
 data_pfx = '/media/data2/nhaber/offline_data'
-HDF5_FILENAMES = [data_pfx + str(i) + '.hdf5' for i in range(1)]#due to seeding issue, the others should in fact be identical.
+#HDF5_FILENAMES = [data_pfx + str(i) + '.hdf5' for i in range(1)]#due to seeding issue, the others should in fact be identical.
+force_scaling = 200
+HDF5_FILENAMES = ['/media/data2/nhaber/scal' + str(int(force_scaling)) + '_' + str(tasknum) + '.hdf5' for tasknum in [2, 4, 5]]
 BATCH_SIZE = 32
-T_PER_STATE = 2
-NUM_TIMESTEPS = 3
-UNIFORM_METADATA_LOC = '/media/data2/nhaber/uniform_metadata.pkl'
-OBJTHERE_METADATA_LOC = '/media/data2/nhaber/objthere_metadata.pkl'
+#T_PER_STATE = 2
+#NUM_TIMESTEPS = 1
+UNIFORM_METADATA_LOC = '/media/data2/nhaber/uniform_scal200.pkl'
+OBJTHERE_METADATA_LOC = '/media/data2/nhaber/objthere_scal200.pkl'
 
 data_lengths = {
-                        'obs' : {'depths1' : T_PER_STATE + NUM_TIMESTEPS},
-                        'action' : T_PER_STATE - 1 + NUM_TIMESTEPS,
-                        'action_post' : T_PER_STATE - 1 + NUM_TIMESTEPS}
+                        'obs' : {'depths1' : 5},
+                        'action' : 4,
+                        'action_post' : 4}
 
 
 
 
-#uniform_metadata = static_data.get_uniform_metadata(HDF5_FILENAMES, UNIFORM_METADATA_LOC, data_lengths)
-#objthere_metadata = static_data.get_objthere_metadata(HDF5_FILENAMES, OBJTHERE_METADATA_LOC, data_lengths)
+print('checking if object there')
+static_data.check_obj_there(HDF5_FILENAMES)
+print('done checking')
 
+uniform_metadata = static_data.get_uniform_metadata(HDF5_FILENAMES, UNIFORM_METADATA_LOC, data_lengths)
+objthere_metadata = static_data.get_objthere_metadata(HDF5_FILENAMES, OBJTHERE_METADATA_LOC, data_lengths)
+
+'''
 dummy_sess = None
 #rand_dp = static_data.OfflineDataProvider(BATCH_SIZE, static_data.UniformRandomBatcher, data_lengths, 5, UNIFORM_METADATA_LOC, batcher_kwargs = {'seed' : 0})
 #rand_dp.start_runner(dummy_sess)
@@ -70,7 +77,7 @@ for i in range(10):
 #	print(batch['action'][:,-1])
 #	print(batch['action_post'][:, -1])
 
-
+'''
 
 
 
